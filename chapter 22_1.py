9b87cd1be2af76f15a8b2b56232d9df1b3f65686
Yashaswi Rahut <yashaswirahut02@gmail.com>
2018-10-30 11:58:39 +0530

    Create chapter 22_1.py

---
+++ chapter 22_1.py
@@ -0,0 +1,18 @@
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+emp = Employee('hani')
+intern = PartTimeEmployee('raj')
+emp.calculate_wage(10)
+intern.calculate_wage(10)
